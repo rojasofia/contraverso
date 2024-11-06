1404ef335cc0835e8ceacf67a6fb6d08b1d46453
SOFIA VITERY <jsofiavm@gmail.com>
2024-11-06 11:36:30 -0500

    Correcion plugins vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-404',
-      writeBundle() {
-        fs.copyFileSync('404.html', 'dist/404.html')
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html'
       }
     }
-  ],
+  },
   base:'https://rojasofia.github.io/contraverso' 
 })
