c50b26c8e19379848c7362f1bdb50289877388e3
SOFIA VITERY <jsofiavm@gmail.com>
2024-11-06 11:06:24 -0500

    Corrigiendo despliege actions

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import  store from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename='/contraverso-project-frontend-7'>
+    <BrowserRouter basename='/contraverso'>
       <AppRouter />
     </BrowserRouter>
   </Provider>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://makaiabootcamp.github.io/contraverso-project-frontend-7' 
+  base:'https://rojasofia.github.io/contraverso' 
 })
