b9ff698ae9e3d19c29310e74cb51c01f76e68d03
SOFIA VITERY <jsofiavm@gmail.com>
2024-11-06 11:30:26 -0500

    Archivo 404 redirecciones

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      writeBundle() {
+        fs.copyFileSync('404.html', 'dist/404.html')
+      }
+    }
+  ],
   base:'https://rojasofia.github.io/contraverso' 
 })
